3d6374c06231948055ab92c2d0272b1faf94d63c
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2023-03-29 11:10:11 +0200

    table en require toegevoegd
    table en require toegevoegd aan de create/index.php

--- tasks/index.php
+++ tasks/index.php
@@ -3,5 +3,42 @@
     <?php require_once '../head.php'; ?>
 </head>
 <body>
+    
+    <div class="container">
     <a href="create.php">Create pagina &gt;</a>
+        <?php if(isset($_GET['msg']))
+        {
+            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
+        } ?>
+
+        <?php
+            require_once '../backend/conn.php';
+            $query = "SELECT * FROM taken";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $taken = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+    <table>
+        <tr>
+            <th>titel</th>
+            <th>beschrijving</th>
+            <th>afdeling</th>
+            <th>status</th>
+            <th>deadline</th>
+            <th>user</th>
+            <th>id</th>
+        </tr>
+        <?php foreach($taken as $taak): ?>
+            <tr>
+                <td><?php echo $taak['titel']; ?></td>
+                <td><?php echo $taak['beschrijving']; ?></td>
+                <td><?php echo $taak['afdeling']; ?></td>
+                <td><?php echo $taak['status']; ?></td>
+                <td><?php echo $taak['deadline']; ?></td>
+                <td><?php echo $taak['user']; ?></td>
+                <td><?php echo "<a href='edit.php?id={$taak['id']}'>" ?>aanpassen</td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    </div>  
 </body>
