d93ce3100af04575d779c2e0a1faf99119b186f0
ColinFaes <colinfaes@gmail.com>
2023-03-31 10:51:39 +0200

    Update edit.php
    Het 5-stappenplan geïmplementeerd.

--- tasks/edit.php
+++ tasks/edit.php
@@ -7,7 +7,18 @@
 </head>
 
 <body>
-    
+    <div class="container">
+        <h1>Taken aanpassen</h1>
+
+        <?php
+        $id = $_GET['id'];
+        require_once '../backend/conn.php';
+        $query = "SELECT * FROM taken WHERE id = :id";
+        $statement = $conn->prepare($query);
+        $statement->execute([":id" => $id]);
+        $melding = $statement->fetch(PDO::FETCH_ASSOC)
+        ?>
+    </div>
 </body>
 
 </html>
