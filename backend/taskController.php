80cb2ede4e136979b87e8d6b161a43c47098fae5
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2023-03-31 10:59:10 +0200

    input validaite toegevoegd aan controller
    inputvalidatie toegevoegd aan alle variable van controller

--- backend/taskController.php
+++ backend/taskController.php
@@ -3,11 +3,41 @@ $action = $_POST['action'];
 
 if($action == "create"){
     $titel = $_POST['titel'];
+    if(empty($titel))
+    {
+        $errors[] = "Vul de title in.";
+    }
     $beschrijving = $_POST['beschrijving'];
+    if(empty($beschrijving))
+    {
+        $errors[] = "Vul een beschrijving in.";
+    }
     $afdeling = $_POST['afdeling'];
+    if(empty($afdeling))
+    {
+        $errors[] = "Vul de afdeling in.";
+    }
     $status = $_POST['status'];
+    if(empty($status))
+    {
+        $errors[] = "Vul de status in.";
+    }
     $deadline = $_POST['deadline'];
+    if(empty($deadline))
+    {
+        $errors[] = "Vul de deadline in.";
+    }
     $user = $_POST['user'];
+    if(!is_numeric($user))
+    {
+        $errors[] = "Vul een geledige user in.";
+    }
+
+    if(isset($errors))
+    {
+        var_dump($errors);
+        die();
+    }
 
     echo $titel . " / " . $beschrijving . " / " . $afdeling . " / " . $status . " / " . $deadline . " / " . $user;
 
