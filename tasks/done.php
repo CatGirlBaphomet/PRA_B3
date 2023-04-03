b7f5680b0a898d5859968b7ed388a6d666ac1267
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2023-04-03 10:47:14 +0200

    link terug in naar to-do
    Een link terug gezet naar de to-do pagina

--- tasks/done.php
+++ tasks/done.php
@@ -5,7 +5,9 @@
 <body>
     
     <div class="container">
+    <a href="index.php">To-Do pagina &gt;</a>
         <h1>Taken die klaar zijn</h1>
+        
         <?php if(isset($_GET['msg']))
         {
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
