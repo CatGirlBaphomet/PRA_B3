8b7d6f534799efb8aeb38b19ce34bcd63bfb242e
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2023-04-03 10:05:57 +0200

    Not Done pagina
    Not done pagina gemaakt

---
+++ tasks/notDone.php
@@ -0,0 +1,43 @@
+<head>
+    <title>Completed pagina</title>
+    <?php require_once '../head.php'; ?>
+</head>
+<body>
+    
+    <div class="container">
+        <?php if(isset($_GET['msg']))
+        {
+            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
+        } ?>
+
+        <?php
+            require_once '../backend/conn.php';
+            $query = "SELECT * FROM taken";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $taken = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+    <table>
+        <tr>
+            <th>titel</th>
+            <th>beschrijving</th>
+            <th>afdeling</th>
+            <th>status</th>
+            <th>deadline</th>
+            <th>user</th>
+        </tr>
+        <?php foreach($taken as $taak){ ?>
+            <?php if ($taak['status'] == "Klaar"){ ?>
+                <tr>
+                    <td><?php echo $taak['titel']; ?></td>
+                    <td><?php echo $taak['beschrijving']; ?></td>
+                    <td><?php echo $taak['afdeling']; ?></td>
+                    <td><?php echo $taak['status']; ?></td>
+                    <td><?php echo $taak['deadline']; ?></td>
+                    <td><?php echo $taak['user']; ?></td>
+                </tr>
+            <?php }?>
+        <?php } ?>
+    </table>
+    </div>  
+</body>
