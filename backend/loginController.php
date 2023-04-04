db8b23b9831a66fd0d0fa1aa5cbe63a02725bf01
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2023-04-04 10:53:06 +0200

    login controller gemaakt
    login controller gemaakt en toegevoegd

---
+++ backend/loginController.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+//1. Haal gegevens van gebruiker op, aan de hand van email/username
+
+//   Vijfstappenplan:
+
+//      1. Haal de verbinding
+
+require_once 'conn.php';
+$query = "SELECT * FROM users WHERE username = :username";
+$statement = $conn ->prepare($query);
+$statement -> execute([":username" => $username]);
+$user = $statement -> fetch(PDO::FETCH_ASSOC);
+
+
+
+
+
+//2. Check of je een resultaat krijgt (anders: account bestaat niet)
+
+//   If-statement, check of "$statement->rowCount()" kleiner is dan 1
+
+
+
+
+if ($statement -> rowCount() < 1)
+{
+    die("error: account bestaat niet");
+}
+
+if (!password_Verify($password, $user['password']))
+{
+    die("error: wachtwoord niet juist");
+}
+
+$_SESSION['user_id'] = $user['id'];
+
+$_SESSION['user_name'] = $user['naam'];
+
+header("location: ../index.php?");
