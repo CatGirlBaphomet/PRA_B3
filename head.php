66e482214f432029a74fe63ed3ad7ef491136f4e
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2023-04-04 10:53:41 +0200

    login toegevoegd
    login links toegevoegd aan de head

--- head.php
+++ head.php
@@ -5,3 +5,12 @@
 <link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
 <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
 <script src="https://kit.fontawesome.com/90971cfe75.js" crossorigin="anonymous"></script>
+<div class="container">
+    <div class="container1">
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="<?php echo $base_url; ?>/login/logout.php">Uitloggen</a>
+        <?php else: ?>
+            <a href="<?php echo $base_url; ?>/login/login.php">Inloggen</a>
+        <?php endif; ?>
+    </div>
+</div>
