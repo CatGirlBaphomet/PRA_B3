998b94236c2d90d9603afdd811f38bcdb0ff581a
ColinFaes <colinfaes@gmail.com>
2023-04-17 10:35:15 +0200

    opmaak
    Wat wijzigingen in de opmaak gemaakt

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <img src="<?php echo $base_url; ?>/img/logo-big-v4.png" alt="Logo van DeveloperLand">
         <a href="<?php echo $base_url; ?>/index.php"><i class="fa-solid fa-house"></i> Home pagina</a>
         <a href="<?php echo $base_url; ?>/tasks/index.php"><i class="fa-solid fa-list-check"></i> Taken Lijst</a>
-        <a href="<?php echo $base_url; ?>/tasks/usertask.php">Jouw Taken</a>
+        <a href="<?php echo $base_url; ?>/tasks/usertask.php"><i class="fa-solid fa-person"></i> Jouw Taken</a>
         <a href="<?php echo $base_url; ?>/tasks/done.php"><i class="fa-solid fa-check"></i> Voltooide Taken</a>
         <?php if (isset($_SESSION['user_id'])): ?>
             <a href="<?php echo $base_url; ?>/logout.php"><i class="fa-solid fa-right-from-bracket"></i> Uitloggen</a>
