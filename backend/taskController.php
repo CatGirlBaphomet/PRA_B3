6051e3d53b6dd1e70c7de10c597380d5c8ec7728
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2023-04-17 10:34:41 +0200

    user aan taskcontroller toegevoegd
    heb de user aan taskcontroller edit toegevoegd

--- backend/taskController.php
+++ backend/taskController.php
@@ -116,10 +116,11 @@ if($action == "edit")
     $status = $_POST['status'];
     $deadline = $_POST['deadline'];
     $color = $_POST['color'];
+    $user = $_POST['user'];
     $id = $_POST['id'];
 
     require_once 'conn.php';
-    $query = "UPDATE taken SET titel = :titel, beschrijving = :beschrijving, afdeling = :afdeling, status = :status, deadline = :deadline, color = :color WHERE id = :id";
+    $query = "UPDATE taken SET titel = :titel, beschrijving = :beschrijving, afdeling = :afdeling, status = :status, deadline = :deadline, color = :color, user = :user WHERE id = :id";
     
     $statement = $conn->prepare($query);
     
@@ -130,6 +131,7 @@ if($action == "edit")
         ":status" => $status,
         ":deadline" => $deadline,
         ":color" => $color,
+        ":user"=>$user,
         ":id" => $id
     ]);
 
